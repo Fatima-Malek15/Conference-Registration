e7e3132bddcec6df5f0d43a751fd6cd07598e691
Fatima <fathimamalek1@gmail.com>
2025-09-07 19:16:04 +0200

    added decl and impl to RegistrationList class Part 1

--- registrationlist.cpp
+++ registrationlist.cpp
@@ -1,3 +1,53 @@
 #include "registrationlist.h"
 
 RegistrationList::RegistrationList() {}
+
+bool RegistrationList::addRegistration(Registration *r)
+{
+    if(r != NULL){
+        m_AttendeeList.append(r->Registration());
+        return true;
+    }
+    return false;
+}
+
+RegistrationList::~RegistrationList()
+{
+    if(!m_AttendeeList.isEmpty())
+    {
+        m_AttendeeList.clear();
+    }
+}
+
+bool RegistrationList::isRegistered()
+{
+    if(!m_AttendeeList.isEmpty())
+    {
+        for(Registration *r:m_AttendeeList){
+            if(m_AttendeeList.contains(r->getAttendee()))
+            {
+                QString("Attendee %1 already registered").arg(r->getAttendee());
+                return true;
+            }
+            else
+            {
+                QString("Attendee %1 not registered").arg(r->getAttendee());
+            }
+
+        }
+    }
+    return false;
+}
+
+double RegistrationList::totalFee(QString l)
+{
+    if(l == "StudentRegistration")
+    {
+
+    }
+}
+
+int RegistrationList::totalRegistration(QString a)
+{
+
+}
--- registrationlist.h
+++ registrationlist.h
@@ -1,10 +1,20 @@
 #ifndef REGISTRATIONLIST_H
 #define REGISTRATIONLIST_H
 
-class RegistrationList
+#include "registration.h"
+
+class RegistrationList : public Registration
 {
 public:
     RegistrationList();
+    bool addRegistration(Registration* r);
+    ~RegistrationList();
+    bool isRegistered();
+    double totalFee(QString l);
+    int totalRegistration(QString a);
+
+private:
+    QList<Registration*> m_AttendeeList;
 };
 
 #endif // REGISTRATIONLIST_H
